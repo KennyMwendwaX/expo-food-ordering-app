44038cf38a76bfb4dc3faa590394544456b2323f
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-03-27 23:02:18 +0300

    feat: add screen options for the MenuStack

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Link, Tabs } from "expo-router";
 import { Pressable } from "react-native";
@@ -34,20 +33,6 @@ export default function TabLayout() {
           headerShown: false,
           tabBarIcon: ({ color }) => (
             <TabBarIcon name="cutlery" color={color} />
-          ),
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? "light"].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
           ),
         }}
       />
---
+++ src/app/cart.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function CartScreen() {
+  return (
+    <View>
+      <Text>cart</Text>
+    </View>
+  );
+}
