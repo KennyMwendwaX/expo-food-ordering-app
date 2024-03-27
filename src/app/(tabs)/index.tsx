45d8e33829cf88a2bab1a7881cb503658a3917d5
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-03-27 09:49:16 +0300

    feat: add types.ts for product types

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,37 +1,11 @@
-import { StyleSheet, View, Text, Image } from "react-native";
-import Colors from "@/constants/Colors";
+import { View } from "react-native";
 import products from "../../../assets/data/products";
-
-const product = products[0];
+import ProductListItem from "@/components/ProductListItem";
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem products={products} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 20,
-  },
-  image: {
-    width: "100%",
-    aspectRatio: 1,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "600",
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: "bold",
-  },
-});
