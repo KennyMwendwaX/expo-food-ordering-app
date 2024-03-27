7da4e91d2e36c28a4bfd410b3cb3520cd7b25675
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-03-27 13:04:36 +0300

    feat: create dynamic ProductDetailsScreen

--- src/app/[id].tsx
+++ src/app/[id].tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import { useLocalSearchParams } from "expo-router";
 import { View, Text } from "react-native";
 
 export default function ProductDetailsScreen() {
+  const { id } = useLocalSearchParams();
+
   return (
     <View>
-      <Text>ProductDetails</Text>
+      <Text style={{ fontSize: 20 }}>ProductDetails for id: {id}</Text>
     </View>
   );
 }
