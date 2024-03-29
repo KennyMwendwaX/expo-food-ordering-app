cd92c9ba2aa2f0a388b0cc0c35a555f9ce3a6314
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-03-29 23:44:58 +0300

    feat: add index.tsx entry screen to switch between user & admin modes

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import Button from "@/components/Button";
+import { Link } from "expo-router";
+import { View } from "react-native";
+
+export default function index() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", padding: 10 }}>
+      <Link href={"/(user)"} asChild>
+        <Button text="User" />
+      </Link>
+      <Link href={"/(admin)"} asChild>
+        <Button text="Admin" />
+      </Link>
+    </View>
+  );
+}
