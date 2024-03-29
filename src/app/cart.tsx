7258c9ec2ff5451c34a97ecb2c2d9c801c32ad1d
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-03-29 22:47:30 +0300

    feat: add checkout button

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -3,17 +3,20 @@ import { useContext } from "react";
 import { View, Text, Platform, FlatList } from "react-native";
 import { useCart } from "./providers/CartProvider";
 import CartListItem from "@/components/CartListItem";
+import Button from "@/components/Button";
 
 export default function CartScreen() {
   const { items } = useCart();
   return (
-    <View>
+    <View style={{ padding: 10 }}>
       <FlatList
         data={items}
         renderItem={({ item }) => <CartListItem cartItem={item} />}
         keyExtractor={(item) => item.id}
-        contentContainerStyle={{ padding: 10, gap: 10 }}
+        contentContainerStyle={{ gap: 10 }}
       />
+
+      <Button text="Checkout" />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
