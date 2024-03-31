591b04fc3c1e7f022231d249419b311b790d0d03
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-03-31 20:13:29 +0300

    feat: add name and price form fields

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,9 +1,41 @@
-import { View, Text, StyleSheet } from "react-native";
+import Button from "@/components/Button";
+import { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 export default function CreateProductScreen() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const resetFields = () => {
+    setName("");
+    setPrice("");
+  };
+
+  const onCreate = () => {
+    console.warn("Creating product: ", name);
+    resetFields();
+  };
+
   return (
     <View style={styles.container}>
-      <Text> create</Text>
+      <Text style={styles.label}>Name</Text>
+      <TextInput
+        value={name}
+        onChangeText={setName}
+        placeholder="Name"
+        style={styles.input}
+      />
+
+      <Text style={styles.label}>Price</Text>
+      <TextInput
+        value={price}
+        onChangeText={setPrice}
+        placeholder="9.99"
+        style={styles.input}
+        keyboardType="numeric"
+      />
+
+      <Button text="Create" onPress={onCreate} />
     </View>
   );
 }
@@ -13,5 +45,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     padding: 10,
+  },
+  label: {
+    color: "gray",
+  },
+  input: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 5,
+    marginBottom: 20,
   },
 });
