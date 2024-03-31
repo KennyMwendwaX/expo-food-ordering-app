86364412d14f3e122a8a4949f1bd437b5a7801a1
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-03-31 22:11:22 +0300

    refactor: create new component for Input for input control

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,6 +1,12 @@
 import Button from "@/components/Button";
-import { useForm } from "react-hook-form";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { Control, useController, useForm } from "react-hook-form";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  KeyboardTypeOptions,
+} from "react-native";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 
@@ -12,21 +18,50 @@ const formValuesSchema = z.object({
     })
     .min(3, { message: "Name must be greater than 3 characters long" })
     .max(20, { message: "Name must be less than 20 characters long" }),
-  price: z
-    .number({ required_error: "Price is required" })
-    .min(1, { message: "Price must be greater than Ksh. 1" }),
+  price: z.string({
+    required_error: "Price is required",
+  }),
 });
 
 type FormValues = z.infer<typeof formValuesSchema>;
 
+type InputProps = {
+  name: keyof FormValues;
+  placeholder: string;
+  keyboardType?: KeyboardTypeOptions;
+  control: Control<FormValues>;
+};
+
+function Input({ name, placeholder, keyboardType, control }: InputProps) {
+  const { field } = useController({
+    control,
+    defaultValue: "",
+    name,
+  });
+
+  const stringValue = field.value.toString();
+
+  return (
+    <TextInput
+      value={stringValue}
+      onChangeText={field.onChange} // Ensure onChange can handle string input and convert it back if necessary
+      placeholder={placeholder}
+      keyboardType={keyboardType}
+      style={styles.input}
+    />
+  );
+}
+
 export default function CreateProductScreen() {
   const {
-    register,
+    control,
     handleSubmit,
     formState: { errors },
   } = useForm<FormValues>({
     resolver: zodResolver(formValuesSchema),
   });
+
+  console.log(errors);
 
   const onCreate = (values: FormValues) => {
     console.warn("Creating product", values);
@@ -34,21 +69,22 @@ export default function CreateProductScreen() {
 
   return (
     <View style={styles.container}>
+      {errors.name && (
+        <Text style={styles.errorMessage}>{errors.name.message}</Text>
+      )}
+      {errors.price && (
+        <Text style={styles.errorMessage}>{errors.price.message}</Text>
+      )}
+
       <Text style={styles.label}>Name</Text>
-      <TextInput
-        placeholder="Name"
-        style={styles.input}
-        {...register("name")}
-      />
-
+      <Input name="name" placeholder="Name" control={control} />
       <Text style={styles.label}>Price</Text>
-      <TextInput
+      <Input
+        name="price"
         placeholder="9.99"
-        style={styles.input}
         keyboardType="numeric"
-        {...register("price")}
+        control={control}
       />
-
       <Button text="Create" onPress={handleSubmit(onCreate)} />
     </View>
   );
@@ -69,5 +105,9 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     marginTop: 5,
     marginBottom: 20,
+  },
+  errorMessage: {
+    color: "red",
+    marginBottom: 10,
   },
 });
