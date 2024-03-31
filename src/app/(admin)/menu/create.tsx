9639033053f8a96dcc902f7a30ed433e3ac9bb5a
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-03-31 20:43:13 +0300

    feat: add resolver and zod for input validation

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -1,19 +1,32 @@
 import Button from "@/components/Button";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { View, Text, StyleSheet, TextInput } from "react-native";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type FormValues = {
-  name: string;
-  price: number;
-};
+const formValuesSchema = z.object({
+  name: z
+    .string({
+      required_error: "Name is required",
+      invalid_type_error: "Name must be a string",
+    })
+    .min(3, { message: "Name must be greater than 3 characters long" })
+    .max(20, { message: "Name must be less than 20 characters long" }),
+  price: z
+    .number({ required_error: "Price is required" })
+    .min(1, { message: "Price must be greater than Ksh. 1" }),
+});
+
+type FormValues = z.infer<typeof formValuesSchema>;
 
 export default function CreateProductScreen() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormValues>();
+  } = useForm<FormValues>({
+    resolver: zodResolver(formValuesSchema),
+  });
 
   const onCreate = (values: FormValues) => {
     console.warn("Creating product", values);
