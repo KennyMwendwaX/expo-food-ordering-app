d70a79c0b421fb3743c46604df6039ec59e96a3e
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-04-09 23:55:12 +0300

    refactor: move index.tsx file to list dir

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -1,15 +1,15 @@
-import { FlatList } from "react-native";
-import orders from "../../../../assets/data/orders";
+import { FlatList, View } from "react-native";
+import orders from "../../../../../assets/data/orders";
 import OrderListItem from "@/components/OrderListItem";
 
 export default function OrdersScreen() {
   return (
-    <>
+    <View>
       <FlatList
         data={orders}
         contentContainerStyle={{ gap: 10, padding: 10 }}
         renderItem={({ item }) => <OrderListItem order={item} />}
       />
-    </>
+    </View>
   );
 }
