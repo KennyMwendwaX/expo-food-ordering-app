93259a3cb95674f4d9156883a6ddc71482f7b10b
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-04-14 23:19:17 +0300

    feat: add product zod schema

--- backend/lib/schema.ts
+++ backend/lib/schema.ts
@@ -21,3 +21,22 @@ export const signupSchema = z.object({
     })
     .min(8, { message: "Password must be greater than 8 characters long" }),
 });
+
+const productSchema = z.object({
+  name: z
+    .string({
+      required_error: "Name is required",
+      invalid_type_error: "Name must be a string",
+    })
+    .min(3, { message: "Name must be greater than 3 characters long" })
+    .max(20, { message: "Name must be less than 20 characters long" }),
+  price: z.string({
+    required_error: "Price is required",
+  }),
+  imageUrl: z
+    .string({
+      required_error: "Image is required",
+      invalid_type_error: "Invalid image URL",
+    })
+    .url({ message: "Invalid image URL format" }),
+});
